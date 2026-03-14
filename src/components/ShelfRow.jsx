79944edcb892fbb76c3c4ede29c2e3124723a65b
Boyan Yanchev <byanchev6@gmail.com>
2026-03-15 00:28:46 +0200

    Removed shelf look, it is now simplified

--- src/components/ShelfRow.jsx
+++ src/components/ShelfRow.jsx
@@ -2,14 +2,12 @@ import CertificateCard from './CertificateCard';
 
 export default function ShelfRow({ certificates, onOpen }) {
   return (
-    <div className="mb-12">
-      <div className="grid grid-cols-4 gap-6 px-4">
+    <div className="mb-10">
+      <div className="grid grid-cols-4 gap-6 px-6">
         {certificates.map((cert) => (
           <CertificateCard key={cert.id} cert={cert} onOpen={onOpen} />
         ))}
       </div>
-      {/* Shelf plank */}
-      <div className="h-4 bg-white rounded mx-2 shadow-[0_8px_20px_rgba(0,0,0,0.18)]" />
     </div>
   );
 }
