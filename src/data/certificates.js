e23d03ebee5576b38321eaa6204aa88bde7cf9b7
Boyan Yanchev <byanchev6@gmail.com>
2026-03-14 22:26:41 +0200

    Update image paths to proper Vite static imports

--- src/data/certificates.js
+++ src/data/certificates.js
@@ -1,44 +1,62 @@
+import progBasics1 from '../assets/certs/Programming Basics - January 2025 - Certificate-1.png';
+import progBasics2 from '../assets/certs/Programming Basics - January 2025 - Certificate-2.png';
+
+import progFund1 from '../assets/certs/Programming Fundamentals with JavaScript - May 2025 - Certificate-1.png';
+import progFund2 from '../assets/certs/Programming Fundamentals with JavaScript - May 2025 - Certificate-2.png';
+import progFund3 from '../assets/certs/Programming Fundamentals with JavaScript - May 2025 - Certificate-3.png';
+
+import jsAdv1 from '../assets/certs/JS Advanced - September 2025 - Certificate-1.png';
+import jsAdv2 from '../assets/certs/JS Advanced - September 2025 - Certificate-2.png';
+
+import jsApps1 from '../assets/certs/JS Applications - October 2025 - Certificate-1.png';
+import jsApps2 from '../assets/certs/JS Applications - October 2025 - Certificate-2.png';
+
+import htmlCss1 from '../assets/certs/HTML & CSS - January 2026 - Certificate-1.png';
+import htmlCss2 from '../assets/certs/HTML & CSS - January 2026 - Certificate-2.png';
+
+import seoFoundations from '../assets/certs/CertificateOfCompletion_SEO Foundations.png';
+
 export const certificates = [
     {
         id: 1,
         title: "Programming Basics",
         issuer: "SoftUni",
         date: "17.02.2025",
-        images: ["../assets/certs/Programming Basics - January 2025 - Certificate-1.png", "../assets/certs/Programming Basics - January 2025 - Certificate-2.png"]
+        images: [progBasics1, progBasics2]
     },
     {
         id: 2,
         title: "Programming Fundamentals with JavaScript",
         issuer: "SoftUni",
         date: "05.08.2025",
-        images: ["../assets/certs/Programming Fundamentals with JavaScript - May 2025 - Certificate-1.png", "../assets/certs/Programming Fundamentals with JavaScript - May 2025 - Certificate-2.png", "../assets/certs/Programming Fundamentals with JavaScript - May 2025 - Certificate-3.png"]
+        images: [progFund1, progFund2, progFund3]
     },
     {
         id: 3,
         title: "JS Advanced",
         issuer: "SoftUni",
         date: "20.10.2025",
-        images: ["../assets/certs/JS Advanced - September 2025 - Certificate-1.png", "../assets/certs/JS Advanced - September 2025 - Certificate-2.png"]
+        images: [jsAdv1, jsAdv2]
     },
     {
         id: 4,
         title: "JS Applications",
         issuer: "SoftUni",
         date: "08.12.2025",
-        images: ["../assets/certs/JS Applications - October 2025 - Certificate-1.png", "../assets/certs/JS Applications - October 2025 - Certificate-2.png"]
+        images: [jsApps1, jsApps2]
     },
     {
         id: 5,
         title: "HTML & CSS",
         issuer: "SoftUni",
         date: "02.03.2026",
-        images: ["../assets/certs/HTML & CSS - January 2026 - Certificate-1.png", "../assets/certs/HTML & CSS - January 2026 - Certificate-2.png"]
+        images: [htmlCss1, htmlCss2]
     },
     {
         id: 6,
         title: "SEO Foundations",
         issuer: "LinkedIn Learning",
         date: "25.03.2024",
-        images: ["../assets/certs/CertificateOfCompletion_SEO Foundations.png"]
+        images: [seoFoundations]
     }
 ];
