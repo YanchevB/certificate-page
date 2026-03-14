463e00249251ec2c164bf21959beaf81210852c3
Boyan Yanchev <byanchev6@gmail.com>
2026-03-14 21:13:11 +0200

    Add certificate data

---
+++ src/data/certificates.js
@@ -0,0 +1,44 @@
+export const certificates = [
+    {
+        id: 1,
+        title: "Programming Basics",
+        issuer: "SoftUni",
+        date: "17.02.2025",
+        images: ["../assets/certs/Programming Basics - January 2025 - Certificate-1.png", "../assets/certs/Programming Basics - January 2025 - Certificate-2.png"]
+    },
+    {
+        id: 2,
+        title: "Programming Fundamentals with JavaScript",
+        issuer: "SoftUni",
+        date: "05.08.2025",
+        images: ["../assets/certs/Programming Fundamentals with JavaScript - May 2025 - Certificate-1.png", "../assets/certs/Programming Fundamentals with JavaScript - May 2025 - Certificate-2.png", "../assets/certs/Programming Fundamentals with JavaScript - May 2025 - Certificate-3.png"]
+    },
+    {
+        id: 3,
+        title: "JS Advanced",
+        issuer: "SoftUni",
+        date: "20.10.2025",
+        images: ["../assets/certs/JS Advanced - September 2025 - Certificate-1.png", "../assets/certs/JS Advanced - September 2025 - Certificate-2.png"]
+    },
+    {
+        id: 4,
+        title: "JS Applications",
+        issuer: "SoftUni",
+        date: "08.12.2025",
+        images: ["../assets/certs/JS Applications - October 2025 - Certificate-1.png", "../assets/certs/JS Applications - October 2025 - Certificate-2.png"]
+    },
+    {
+        id: 5,
+        title: "HTML & CSS",
+        issuer: "SoftUni",
+        date: "02.03.2026",
+        images: ["../assets/certs/HTML & CSS - January 2026 - Certificate-1.png", "../assets/certs/HTML & CSS - January 2026 - Certificate-2.png"]
+    },
+    {
+        id: 6,
+        title: "SEO Foundations",
+        issuer: "LinkedIn Learning",
+        date: "25.03.2024",
+        images: ["../assets/certs/CertificateOfCompletion_SEO Foundations.png"]
+    }
+];
